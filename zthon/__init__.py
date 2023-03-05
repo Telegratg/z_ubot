0e05d77b53e94f2671e71839d6f20399b73cde9e
Telegratg <101783603+Telegratg@users.noreply.github.com>
2023-03-06 02:28:50 +0300

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -14,7 +14,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.1.1"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "ZThon <https://github.com/Zed-Thon/z_ubot>"
+__author__ = "ZThon <https://github.com/Telegratg/z_ubot>"
 __copyright__ = f"ZThon Copyright (C) 2021 - 2022  {__author__}"
 
 zedub.version = __version__
